4bb122c1e14a9f1a21b1ae5c8294060e54166c9b
VasiliyK <topsecret6261177aa@gmail.com>
2022-12-26 14:06:21 +0300

    feat: header adaptive

--- Plants/src/js/main.ts
+++ Plants/src/js/main.ts
@@ -1,2 +1,26 @@
 import '../scss/main.scss';
 import '../index.html';
+
+function mobileMenu() {
+  const mobileBtn = document.querySelector('#mobileBtn') as HTMLButtonElement;
+  const headerNav = document.querySelector('#headerNav') as HTMLElement;
+
+  const input = () => {
+    setTimeout(() => headerNav.classList.toggle('nav_active'), 10);
+    headerNav.style.display = 'flex';
+    mobileBtn.classList.toggle('mobileBtn_active')
+  }
+
+  const output = () => {
+    setTimeout(() => headerNav.style.display = 'none', 300);
+    mobileBtn.classList.toggle('mobileBtn_active');
+    headerNav.classList.toggle('nav_active');
+  }
+
+  mobileBtn.onclick = () => {
+    if (headerNav.className.includes('nav_active')) {
+      output()
+    } else input()
+  }
+}
+mobileMenu()
