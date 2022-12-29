171445b7818d2950c7875983bfa48c7f08ea1764
VasiliyK <topsecret6261177aa@gmail.com>
2022-12-29 15:06:18 +0300

    fix: scroll to sections

--- Plants/src/js/main.ts
+++ Plants/src/js/main.ts
@@ -1,6 +1,29 @@
 import '../scss/main.scss';
 import '../index.html';
 
+const mobileBtn = document.querySelector('#mobileBtn') as HTMLButtonElement;
+const headerNav = document.querySelector('#headerNav') as HTMLElement;
+
+const input = () => {
+  setTimeout(() => headerNav.classList.toggle('nav_active'), 10);
+  headerNav.style.display = 'flex';
+  mobileBtn.classList.toggle('mobileBtn_active')
+}
+
+const output = () => {
+  setTimeout(() => headerNav.style.display = 'none', 300);
+  mobileBtn.classList.toggle('mobileBtn_active');
+  headerNav.classList.toggle('nav_active');
+}
+
+mobileBtn.onclick = () => {
+  if (headerNav.className.includes('nav_active')) {
+    output()
+  } else input()
+}
+
+// smoth anchors
+
 const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (const anchor of anchors) {
@@ -11,29 +34,9 @@ for (const anchor of anchors) {
       behavior:'smooth',
       block:'start',
     });
+
+    if (headerNav.className.includes('nav_active') && window.outerWidth < 768) {
+      output()
+    } else return
   });
 }
-
-function mobileMenu() {
-  const mobileBtn = document.querySelector('#mobileBtn') as HTMLButtonElement;
-  const headerNav = document.querySelector('#headerNav') as HTMLElement;
-
-  const input = () => {
-    setTimeout(() => headerNav.classList.toggle('nav_active'), 10);
-    headerNav.style.display = 'flex';
-    mobileBtn.classList.toggle('mobileBtn_active')
-  }
-
-  const output = () => {
-    setTimeout(() => headerNav.style.display = 'none', 300);
-    mobileBtn.classList.toggle('mobileBtn_active');
-    headerNav.classList.toggle('nav_active');
-  }
-
-  mobileBtn.onclick = () => {
-    if (headerNav.className.includes('nav_active')) {
-      output()
-    } else input()
-  }
-}
-mobileMenu()
